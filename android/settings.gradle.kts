4bc034971c33c52c295b3e7b2c28affa2fd01fe0
VICTOR MANUEL ITURBIDE MARIANO <adrianagreste18@gmail.com>
2026-09-03 13:34:31 -0600

    MODIFICACION DE VERSION DE PLUGIN DE ANDROID Y AGP

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,12 +1,14 @@
 pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        val localPropertiesFile = java.io.File(rootDir, "local.properties")
+        if (localPropertiesFile.exists()) {
+            localPropertiesFile.reader().use { properties.load(it) }
         }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        assert(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        flutterSdkPath
+    }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
@@ -19,8 +21,10 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.4.0" apply false
+    // Usamos la versión mínima requerida por tu Flutter SDK (8.11.1)
+    id("com.android.application") version "8.11.1" apply false
+    // Declaramos explícitamente Kotlin Gradle Plugin (KGP)
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
 }
 
 include(":app")
