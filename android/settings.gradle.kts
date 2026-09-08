d3ed55eeaa199b944673c6e99b2d2cb688357338
VICTOR MANUEL ITURBIDE MARIANO <adrianagreste18@gmail.com>
2026-09-08 13:34:05 -0600

    CORRECCION DE GRADLE KTS

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,5 +1,4 @@
 pluginManagement {
-<<<<<<< HEAD
     val flutterSdkPath =
         run {
             val properties = java.util.Properties()
@@ -8,17 +7,6 @@ pluginManagement {
             require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
             flutterSdkPath
         }
-=======
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        val localPropertiesFile = java.io.File(rootDir, "local.properties")
-        if (localPropertiesFile.exists()) {
-            localPropertiesFile.reader().use { properties.load(it) }
-        }
-        properties.getProperty("flutter.sdk")
-            ?: error("flutter.sdk not set in android/local.properties")
-    }
->>>>>>> c77126d02bbecfd46df322bb41ea0935bba4d1d3
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
@@ -31,18 +19,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-<<<<<<< HEAD
     id("com.android.application") version "9.1.0" apply false
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
 }
 
 include(":app")
-=======
-    // Usamos la versión mínima requerida por tu Flutter SDK (8.11.1)
-    id("com.android.application") version "9.0.1" apply false
-    // Declaramos explícitamente Kotlin Gradle Plugin (KGP)
-    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
-}
-
-include(":app")
->>>>>>> c77126d02bbecfd46df322bb41ea0935bba4d1d3
